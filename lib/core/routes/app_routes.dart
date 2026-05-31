41426203c09ca06fa8a7dc83401122790aeb4523
notdanna <afrancor1900@alumno.ipn.mx>
2026-05-30 19:23:47 -0600

    feat: estructura base con arquitectura limpia

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -14,6 +14,7 @@ import '../../features/auth/presentation/pages/login_page.dart';
 import '../../features/auth/presentation/pages/reset_password_page.dart';
 import '../../features/shell/presentation/pages/public_shell_page.dart';
 import '../../features/shell/presentation/pages/admin_shell_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AppRoutes {
   AppRoutes._();
@@ -25,8 +26,10 @@ class AppRoutes {
   static const String resetPassword = '/reset-password';
 
   static AuthRepositoryImpl _buildRepo() => AuthRepositoryImpl(
-        remoteDataSource: AuthRemoteDataSourceImpl(client: http.Client()),
-      );
+    remoteDataSource: AuthRemoteDataSourceImpl(
+      client: Supabase.instance.client,
+    ),
+  );
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://dmbpvqhftfbpcuqawvyi.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtYnB2cWhmdGZicGN1cWF3dnlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNzMwNjcsImV4cCI6MjA5NTc0OTA2N30.IAKOSOiOkC3sr9UpbOHFbpyXVBSvtVLEFq3eJSP_2A8',
+  );
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const EtsApp());
 }
